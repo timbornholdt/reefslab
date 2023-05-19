0e66ff085852e979ccb596b7a0e2d222988fbac9
Tim Bornholdt <tim@jedmahonisgroup.com>
2023-05-19 11:46:47 -0500

    Initial Commit

---
+++ Reefslab/ReefslabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReefslabApp.swift
+//  Reefslab
+//
+//  Created by Tim Bornholdt on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ReefslabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
